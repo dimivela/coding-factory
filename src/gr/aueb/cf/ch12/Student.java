2110d99b724eaa9e834de53914fd729f06287dfd
D. Velaoras <velaorasdimi@gmail.com>
2025-02-25 20:36:19 +0200

    commit no2 25/2/2025

--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -1,16 +1,36 @@
 package gr.aueb.cf.ch12;
 
+/**
+ * Data class.
+ * POJO (Plain Old Java Object)
+ * Java Bean
+ */
 public class Student {
+    private static int studentsCount = 0;
+
     private int id;
     private String firstname;
     private String  lastname;
 
-    //Getter
+    //Default Constractor
+    public Student() {
+    }
+
+    //Overloaded Constractor
+    public Student(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public static int getStudentsCount() {
+        return studentsCount;
+    }
+
     public int getId() {
         return id;
     }
 
-    //Setter
     public void setId(int id) {
         this.id = id;
     }
@@ -30,4 +50,38 @@ public class Student {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+
+    //    //Default Constractor
+//    public Student() {
+//        id = 0;
+//        firstname = null;
+//        lastname = null;
+//    }
+//
+//    //Getter
+//    public int getId() {
+//        return id;
+//    }
+//
+//    //Setter
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
 }
--- src/gr/aueb/cf/ch12/StudentDemo.java
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -4,13 +4,25 @@ public class StudentDemo {
 
     public static void main(String[] args) {
         Student student = new Student();      // Student() is a *default* constractor, a constractor without any parameters.
+        Student bob = new Student(2, "Bob", "D.");  //Using the Overloaded Student Constractor.
 
-        student.id = 1;
-        student.firstname = "Alice";
-        student.lastname = "W.";
 
-        System.out.println("Id: " + student.id);
-        System.out.println("Firstname: " + student.firstname);
-        System.out.println("Lastname: " + student.lastname);
+        student.setId(1);               //student.id = 1;
+        student.setFirstname("Alice");  //student.firstname = "Alice";
+        student.setLastname("W.");      //student.lastname = "W.";
+
+        System.out.println("Id: " + student.getId());
+        System.out.println("Firstname: " + student.getFirstname());
+        System.out.println("lastname: " + student.getLastname());
+
+        System.out.println("Id: " + bob.getId());
+        System.out.println("Firstname: " + bob.getFirstname());
+        System.out.println("lastname: " + bob.getLastname());
+
+        System.out.println("Student instances' count: " + Student.getStudentsCount());
+
+//        System.out.println("Id: " + student.id);
+//        System.out.println("Firstname: " + student.firstname);
+//        System.out.println("Lastname: " + student.lastname);
     }
 }
